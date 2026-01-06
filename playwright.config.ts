a12a745189da006ad0fa63f82b93286f769b1ddd
Samithri Asanga Kariyawasam <145251608+Samithri-K@users.noreply.github.com>
2026-01-06 09:02:40 +0530

    Update playwright.config.ts

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,1 +1,11 @@
+import { defineConfig } from '@playwright/test';
 
+export default defineConfig({
+  testDir: './tests',
+  use: {
+    baseURL: 'https://example.com',
+    headless: true,
+    screenshot: 'only-on-failure',
+    trace: 'on-first-retry'
+  }
+});
