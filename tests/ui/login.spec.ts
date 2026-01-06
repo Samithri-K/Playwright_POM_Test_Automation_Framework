8689e85fe4bbec02db58c734356b06f195e10aa7
Samithri Asanga Kariyawasam <145251608+Samithri-K@users.noreply.github.com>
2026-01-06 09:06:26 +0530

    Update login.spec.ts
    Updated Invalid login scenario

--- tests/ui/login.spec.ts
+++ tests/ui/login.spec.ts
@@ -1,11 +1,10 @@
-
 import { test } from '@playwright/test';
 import { LoginPage } from '../../pages/login.page';
 import users from '../../test-data/users.json';
 
 test.describe('Login Feature', () => {
 
-  test('Valid user should be able to login successfully', async ({ page }) => {
+  test('Valid user should login successfully', async ({ page }) => {
     const loginPage = new LoginPage(page);
 
     await loginPage.navigate();
@@ -14,6 +13,17 @@ test.describe('Login Feature', () => {
       users.validUser.password
     );
     await loginPage.verifyLoginSuccess();
+  });
+
+  test('Invalid user should see error message', async ({ page }) => {
+    const loginPage = new LoginPage(page);
+
+    await loginPage.navigate();
+    await loginPage.login(
+      users.invalidUser.username,
+      users.invalidUser.password
+    );
+    await loginPage.verifyInvalidLoginError();
   });
 
 });
