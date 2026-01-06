56abdabefa26e485b3dcd8cc76f107bb3c743bbd
Samithri Asanga Kariyawasam <145251608+Samithri-K@users.noreply.github.com>
2026-01-06 09:01:55 +0530

    Update login.spec.ts

--- tests/ui/login.spec.ts
+++ tests/ui/login.spec.ts
@@ -1,1 +1,19 @@
 
+import { test } from '@playwright/test';
+import { LoginPage } from '../../pages/login.page';
+import users from '../../test-data/users.json';
+
+test.describe('Login Feature', () => {
+
+  test('Valid user should be able to login successfully', async ({ page }) => {
+    const loginPage = new LoginPage(page);
+
+    await loginPage.navigate();
+    await loginPage.login(
+      users.validUser.username,
+      users.validUser.password
+    );
+    await loginPage.verifyLoginSuccess();
+  });
+
+});
